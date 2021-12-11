058f80c53981460d86d17a94854207e33d3f3469
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-12-11 22:39:34 +0900

    add index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,65 +1,77 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        frontend
-      </h1>
-      <h2 class="subtitle">
-        My pioneering Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <section class="section no-top-pad">
+    <div
+      id="js_capture_ref"
+      class="OGPMessage"
+    >
+      <span class="your-message">あなたのメッセージ</span>
+      <span v-if="message">
+        {{ message }}
+      </span>
+      <span
+        v-else
+        class="no-input"
+      >
+        入力エリアにメッセージを入れてください
+      </span>
+    </div>
+    <div class="columns is-centered is-mobile">
+      <div class="column is-half-desktop is-full-mobile is-full-tablet">
+        <from>
+          <div class="field">
+            <label class="label">あなたのメッセージ</label>
+            <div class="control">
+              <input
+                v-model="message"
+                class="input"
+                name="message"
+                placeholder="メッセージを入力してください"
+              >
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              //OGPボタンを配置
+            </div>
+          </div>
+        </from>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    handleGenerateOGP(e) {
+
+    },
+  },
+};
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+.your-message {
+  font-size: 14px;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+
+.no-input {
+  color: #ccc;
 }
-.links
-{
-  padding-top: 15px;
+
+.OGPMessage {
+  padding: 20px 0;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  border-radius: 10px;
+  border: 10px solid #55c500;
+  margin-bottom: 30px;
 }
 </style>
